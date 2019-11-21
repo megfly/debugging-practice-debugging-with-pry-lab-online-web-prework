f9a1aefefaf998cc0b28ddf2dc0181bc2424a282
Meghan Flynn <mflynn333@gmail.com>
2019-11-21 23:09:16 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,27 +1,38 @@
 require 'pry'
 
-def person 
+ person 
 class Person
   attr_accessor :name, :candy_lover
-end
+  binding.pry
 
+
+  
+  
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
   end
 
+  
+  
   def tall?
     true
   end
 
+ 
+ 
   def basketball_player?
     false
   end
 
+ 
+ 
   def likes_candy?
     true
   end
 
+ 
+ 
   def ballin_candy_lover?
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
